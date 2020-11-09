a4cf3a2e49e523339f6bf7a437e12d430988d01d
MAUREEN <maureennayebare2@gmail.com>
2020-11-09 11:47:06 +0300

    Stringl

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log('Hello World!');
+var str='Goodness';
+var strlen=str.length;
+document.write(strlen);
+/*so, for this part, since there is no browser to run the code, we use console. log(alerted);*/ 
+/*var x=10;
+var y=3;
+var result=x+y;
+alert(result);
+console.log(alerted);*/
+/*This is to show how numbers operate. isNaN returns true, paseInt returns an integer, var num 23.7568.tofixed returns specific dps*/
+//var num1=isNaN(5);
+//var num2=parseInt('21 days');
+//var num3=parseFloat('34.6533');
+//var num5=('35.54224').toExponential(3);
+//var num6=('3.45'). toString();
+//document.getElementById('demo').InnerHTML=num1+'<br>'+num2+'<br>'+num3+'<br>'+num4+'<br>'+num5+'<br>'+num6;//
+function MyName(){
+     var MyString='Yesterday';
+     console.log('my last letter is;'+MyString[0]);
+}
+MyName();
+var age=21;
+if(age>18){
+alert('User is old enough to enter');
+} 
+console.log(alerted);
+document.getElementById('submit'). addEventListener('click', function(){var age=document.getElementById('age').value;}) 
+    if(age>18){alert('user is legible') ;
+console.log(alerted);
